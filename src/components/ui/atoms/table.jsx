2dd2a5b9a9d7291074cd60b019f0c72af8ab2fa0
Vladimir Lopatin <107243868+21Trew@users.noreply.github.com>
2025-09-16 16:57:45 +0300

    добавил модалку зп водителя

--- src/components/ui/atoms/table.jsx
+++ src/components/ui/atoms/table.jsx
@@ -1,6 +1,6 @@
-export default function UiTable({ columns, data }) {
+export default function UiTable({ columns, data, onRowClick, rowStyle }) {
   return (
-    <table className="ui-table ">
+    <table className="ui-table">
       <thead>
         <tr>
           {columns.map((col, idx) => (
@@ -10,7 +10,11 @@ export default function UiTable({ columns, data }) {
       </thead>
       <tbody>
         {data.map((row, rowIndex) => (
-          <tr key={row.id || rowIndex}>
+          <tr
+            key={row.id || rowIndex}
+            onClick={() => onRowClick?.(row)}   // 🔹 обработчик клика
+            style={{ cursor: onRowClick ? "pointer" : "default", ...rowStyle }}
+          >
             {columns.map((col, colIndex) => (
               <td key={colIndex}>
                 {col.render ? col.render(row) : row[col.accessor]}
